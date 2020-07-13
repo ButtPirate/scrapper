ccbe6ad377b200ce9320c1644c9f286836031a1d
Butt Pirate <buttpirate@tutanota.com>
2020-07-13 16:33:07 +0300

    Send POST requests using org.apache.httpcomponents

--- src/main/java/ru/buttpirate/WebService.java
+++ src/main/java/ru/buttpirate/WebService.java
@@ -1,8 +1,18 @@
 package ru.buttpirate;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.jsoup.Jsoup;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class WebService {
     public static String getPageAsString(String href, String cookie) throws IOException {
@@ -16,6 +26,30 @@ public class WebService {
         }
 
         return html;
+    }
+
+    public static int sendPost(String url, String cookie, Map<String, String> params, Map<String, String> headers) throws IOException {
+        HttpPost post = new HttpPost(url);
+
+        // Post body
+        List<NameValuePair> urlParameters = new ArrayList<>();
+        for (Map.Entry<String, String> param : params.entrySet()) {
+            urlParameters.add(new BasicNameValuePair(param.getKey(), param.getValue()));
+        }
+        post.setEntity(new UrlEncodedFormEntity(urlParameters));
+
+        // Headers
+        post.setHeader("Cookie", cookie);
+        for (Map.Entry<String, String> header : headers.entrySet()) {
+            post.setHeader(header.getKey(), header.getValue());
+        }
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+             CloseableHttpResponse response = httpClient.execute(post)) {
+
+            return response.getStatusLine().getStatusCode();
+        }
+
     }
 
 }
